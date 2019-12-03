6b7379c0c68ebfa58e920c8c6110de4186b1e30f
Julia Aasmaa <my_e_10@hotmail.com>
2019-12-03 10:10:47 +0200

    draft

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const countdown = document.querySelector('.countdown');
+const launchDate = new Date('Dec 24, 2019 00:00:01').getTime();
+console.log(launchDate);
+
+const interval = setInterval(() => {
+    console.log('tick');
+    const now = new Date().getTime();
+    const distance = launchDate - now;
+
+    const days = Math.floor(distance/(1000*60*60*24));
+    const hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+    const mins = Math.floor((distance % (1000*60*60))/(1000*60));
+    const seconds = Math.floor((distance % (1000*60))/1000);
+    console.log(days, " days");
+    console.log(hours, " hours ");
+    console.log(mins, " minutes");
+    console.log(seconds, " seconds");
+
+    countdown.innerHTML = `
+        <div><span>${days} päeva</span></div>
+        <div><span>${hours} tundi</span></div>
+        <div><span>${mins} minutit</span></div>
+        <div><span>${seconds} sekundit</span></div>
+    
+    `;
+
+
+}, 1000);
